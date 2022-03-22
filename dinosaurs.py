5bb7b49e64df14b0f93d205ac3ecc7a50dacd49b
Dye <anthony.dye@ies-co.com>
2022-03-22 14:15:18 -0500

    added dinosaurs file

---
+++ dinosaurs.py
@@ -0,0 +1,11 @@
+
+
+class Dinosaurs:
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = int
+        self.health = int
+        pass
+
+    def attack(self, robot):
+        pass
